cfffbb235b4459018b4caf8a9bc56b1accf1d239
Vihaga Manodya <vihagamanodya2001@gmail.com>
2026-01-19 00:43:20 +0530

    dasd

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://unrepeatable-shanon-undangerously.ngrok-free.dev" : "/api",
   withCredentials: true,
 
 });
