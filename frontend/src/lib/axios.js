ac6b3664b60fa04c3c896158cc07bba3bf4dbe2e
Vihaga Manodya <vihagamanodya2001@gmail.com>
2026-01-19 10:11:26 +0530

    b

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-  withCredentials: true,
 
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "https://wees-chat.vercel.app/api",
+  withCredentials: true,
 });
